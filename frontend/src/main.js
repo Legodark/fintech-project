cdd4fbc4bc087e0fc52c0db0320fe1d516f581d3
Adri <adri@adridw.com>
2020-10-27 22:36:39 +0100

    resolucion de conflictos

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,37 +7,26 @@ import Vuex from 'vuex'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import axios from 'axios'
-<<<<<<< HEAD
 import estilos from '@/assets/style/main.css'
 
-=======
-import VueAxios from  'vue-axios'
-import store from './store/index'
->>>>>>> b56100eb1ed2004b060ee2de03018018b5775df7
 
 
 // rutes
 
 import HomePage from '@/pages/HomePage'
-<<<<<<< HEAD
 import DashBoard from '@/pages/DashBoard'
-=======
 import LoginPage from '@/pages/LoginPage'
 import RegisterPage from '@/pages/RegisterPage'
 import ProfilePage from '@/pages/ProfilePage'
->>>>>>> b56100eb1ed2004b060ee2de03018018b5775df7
 
 const routes = [
 
   { path: "/", name: "Home", component: HomePage },
-<<<<<<< HEAD
   { path: "/dashboard", name: "DashBoard", component: DashBoard },
-=======
   { path: "/login", name: "Login", component: LoginPage },
   { path: "/register", name: "Register", component: RegisterPage },
   { path: "/profile", name: "Profile", component: ProfilePage}
 
->>>>>>> b56100eb1ed2004b060ee2de03018018b5775df7
 ]
 
 const router = new VueRouter({ routes, mode: 'history' })
